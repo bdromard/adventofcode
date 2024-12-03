39c8beb855e043b33efc61ece1b1e6f31736b3c2
Benjamin Dromard <benjamin.dromard@gmail.com>
2024-12-03 19:59:37 +0100

    feat: day3 solved

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,3 +1,16 @@
+use regex::Regex;
 fn main() {
-    println!("Hello, world!");
+    let pattern = Regex::new(r"mul\(\d*,\d*\)").unwrap();
+    let input_file = std::fs::read_to_string("./input").unwrap();
+    let result: i32 = pattern
+        .find_iter(&input_file)
+        .map(|m| {
+            let result = m.as_str().replace("mul(", "").replace(')', "");
+            let split: Vec<&str> = result.split(',').collect();
+            let first_number: i32 = str::parse(split[0]).unwrap();
+            let second_number: i32 = str::parse(split[1]).unwrap();
+            first_number * second_number
+        })
+        .sum();
+    println!("{:?}", result);
 }
